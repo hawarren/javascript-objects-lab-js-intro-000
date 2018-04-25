d0e9697747dc6417fc834425b1d6c6ade894f502
Hanif Warren <hwarrendev@gmail.com>
2018-04-25 17:46:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
  // object[key] = value;
-  object.assign({}, key: value);
+  object.assign({}, {key: value});
   return object;
 }
